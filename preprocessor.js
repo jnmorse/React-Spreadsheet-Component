06d466b880fdc9493e997a4812d444a139ebfdcb
Joseph Morse <tamed.lionheart@gmail.com>
2019-06-09 20:24:38 -0400

    test: fixed some issues with tests
    tests where not running

--- preprocessor.js
+++ preprocessor.js
@@ -1,17 +1,14 @@
 'use strict';
 
-const babel = require('babel-core');
+const babel = require('@babel/core');
 const jestPreset = require('babel-preset-jest');
 
 module.exports = {
   process(src, filename) {
-    if (babel.util.canCompile(filename)) {
-      return babel.transform(src, {
-        filename,
-        presets: [jestPreset],
-        retainLines: true,
-      }).code;
-    }
-    return src;
-  },
-};
+    return babel.transform(src, {
+      filename,
+      presets: [jestPreset],
+      retainLines: true,
+    }).code;
+  }
+}
